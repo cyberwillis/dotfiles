0ef6e3214120130cd61d379e8e2126d29281b06c
Willismar <cyberwillis@gmail.com>
2019-08-20 15:11:51 -0300

    add execition permission to zsh/install.sh file

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,5 @@
-
+#!/bin/bash -e
 sudo apt install -y zsh
 
+export ZSH="$HOME/.dotfiles/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
