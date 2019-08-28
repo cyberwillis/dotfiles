26042f3344d0c7d706e4670f7c5b24d60ab99e05
Willismar <cyberwillis@gmail.com>
2019-08-28 13:31:55 -0300

    fix to wacom tablets

--- wacom/config.zsh
+++ wacom/config.zsh
@@ -1,4 +1,4 @@
 #Disable WACOM pointer when open the terminal
-if [[ "$(echo lsusb | grep Wacom | head -n 1 2> /dev/null)" != "" ]]; then
+if [[ "$(lsusb | grep Wacom | head -n 1 2> /dev/null)" != "" ]]; then
 xsetwacom --set "Wacom Intuos5 touch M Finger touch" Touch off
 fi;
