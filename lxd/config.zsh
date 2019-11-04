0d87bf910b13c5dbf036f362978cda9f38f4e838
Willismar <cyberwillis@gmail.com>
2019-11-04 10:14:24 -0300

    Adds a new config

--- lxd/config.zsh
+++ lxd/config.zsh
@@ -8,14 +8,34 @@ update_lxd()
 
     pushd
 
-    if [[ "$#" -gt 0 ]];then
-        ARG="$1"
-    fi
+    #if [[ "$#" -gt 0 ]];then
+    #    ARG="$1"
+    #fi
 
-    build_liblxc ${ARG};
-    build_lxcfs ${ARG};
-    build_lxd ${ARG};
-    build_juju;
+    #build_liblxc ${ARG};
+    #build_lxcfs ${ARG};
+    #build_lxd_old ${ARG};
+    #build_juju;
+
+    if [[ "$#" -gt 0 ]]; then
+		DATE_="$1"
+		echo "Working on date: ${DATE_}"
+	fi
+
+	do_install_go ;
+	do_install_python_packages;
+	do_install_fs;
+	do_build_criu ${DATE_};
+	do_install_tools ;
+	do_build_libco ${DATE_};
+	do_build_raft; #didnt put date here because old commits have problems
+	do_build_sqlite ${DATE_};
+	do_build_dqlite ${DATE_};
+	do_build_libseccomp; #dont need old commits
+	do_build_libnvidia_container ${DATE_};
+	do_build_lxc "2019-10-24"; #fix against terminal leak memory
+	do_build_lxcfs ${DATE_};
+	do_build_lxd ${DATE_};
 
     popd
 }
